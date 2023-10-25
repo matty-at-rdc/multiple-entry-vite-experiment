2af0afa0d8b394a20ae2e25262877999b3bdf849
Matthew Cale <matt.cale@move.com>
2023-10-25 13:43:38 -0500

    try: this mostly seems to work

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,59 @@ import { resolve } from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 
+const inputNames = ["door_one", "door_two", "door_three"]
+
+export const mapCSSFileNamesGenerateBundle = {
+  name: 'mapCSSFileNamesGenerateBundle',
+  generateBundle(opts, bundle) {
+
+    let cssNamesMap = {}
+
+    Object.entries(bundle).forEach(([fileName, fileInfo]) => {
+      if (inputNames.includes(fileInfo.name)) {        
+        const ambiguous_css_name = [...fileInfo.viteMetadata.importedCss][0]
+        const mapped_css_name = `${fileInfo.name}.css`
+        bundle[ambiguous_css_name].fileName = mapped_css_name
+        bundle[ambiguous_css_name].name = mapped_css_name
+        cssNamesMap[ambiguous_css_name] = mapped_css_name
+      }
+    })
+  },
+}
+
+
+export const mapCSSFileNamesWriteBundle = {
+  name: 'mapCSSFileNamesWriteBundle',
+  writeBundle(opts, bundle) {
+    const cssNamesMap = {}
+
+    Object.keys(bundle).forEach((key) => {
+      const file = bundle[key]
+      if(file.fileName.endsWith(".css")) {
+        cssNamesMap[key] = file.fileName
+      }
+    })
+
+    Object.entries(bundle).forEach(([fileName, fileInfo]) => {
+      const cssFileNames = Object.keys(cssNamesMap)
+      if(fileName.endsWith(".html")) {
+        cssFileNames.forEach((cssFileName) => {
+          if(fileInfo.source.includes(cssFileName)) {
+            console.log(`Pattern: ${cssFileName} found in ${fileName}`);
+            fileInfo.source = fileInfo.source.replace(cssFileName, cssNamesMap[cssFileName])
+          }
+        })
+      }
+    })
+  },
+}
+
 export default defineConfig(({ mode }) => {
   const config = {
     root: "src",
     build: {
       outDir: "../static/",
+      emptyOutDir: true,
       rollupOptions: {
         maxParallelFileOps: 100,
         input: {
@@ -15,13 +63,7 @@ export default defineConfig(({ mode }) => {
           door_three: resolve(__dirname, "src", "DoorThree", "index.html"),
         },
         output: {
-          entryFileNames: "index.js",
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name === "index.css") {
-              return "index.css"
-            }
-            return assetInfo.name
-          },
+          entryFileNames: "[name].js",
         },
       },
       commonjsOptions: { include: [] },
@@ -30,6 +72,8 @@ export default defineConfig(({ mode }) => {
       react({
         include: [/\.jsx?$/, /\.js$/],
       }),
+      mapCSSFileNamesGenerateBundle,
+      mapCSSFileNamesWriteBundle,
     ],
     resolve: {
       alias: {
